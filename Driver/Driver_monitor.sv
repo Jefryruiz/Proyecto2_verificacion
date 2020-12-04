4a8f415aa9afc97666bd45cef48c60e90b65e1a8
Alexander Alvarado Sotela <44080117+alex9612@users.noreply.github.com>
2020-12-03 18:10:29 -0600

    Update Driver_monitor.sv

--- Driver/Driver_monitor.sv
+++ Driver/Driver_monitor.sv
@@ -1,25 +1,21 @@
-class driver #(parameter pck_sz=40, drvs=16, id_drv);
+class driver #(parameter pck_sz=40, drvs=16);
     virtual device #(.pck_sz(pck_sz))vdc; 
     agent_drv_mbx a_d_mbx; 
     drv_chkr_mbx d_c_mbx;
     event drv_done;
     int wait_;
-    int id_drv;
     transaccion #(.pck_sz(pck_sz),.drvs(drvs)) trans;
     
     task run();
         $display("[%g] El driver fué inicializado",$time);
         @(posedge vdc.clk);
-        vdc.rst=1;
-        @(posedge vdc.clk);
         transaccion #(.pck_sz(pck_sz),.drvs(drvs)) trans; 
-        vdc.dato=pck_sz´bx;
+        vdc.dato=pck_sz´b0;
         $display("[%g] el driver espera por una transacción",$time);
         wait_=0; 
         @(posedge vdc.clk); 
         a_d_mbx.get(trans);
-        dvc=trans.sender;
-        trans.print("Se recibió una transaccion en el driver, dispositivo=%0d ", dvc);
+        trans.print("Se recibió una transaccion en el driver, dispositivo=%0d ", trans.sender);
         while(wait_ < trans.ret)begin
             @(posedge vdc.clk);
             wait_=wait_+1;
@@ -41,7 +37,7 @@ class driver #(parameter pck_sz=40, drvs=16, id_drv);
                 drv_chkr_mbx.put(trans);
                 trans.print("Driver: Transaccion ejecutada";$time);
         default: begin
-            $display("Un error de ambiente, id de dispositivo de envio/llegada no coincide",$time);
+            $display("Error, no existe modo válido de envío/lectura de la transaccion",$time);
             $finish;
         @(drv_done);
         @(posedge vdc.clk);
