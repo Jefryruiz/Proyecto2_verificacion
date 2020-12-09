9526d99cbe77987aa83058a8574d26f9cefc25f7
Alexander Alvarado Sotela <44080117+alex9612@users.noreply.github.com>
2020-12-09 11:49:54 -0600

    Update Driver_monitor.sv

--- Driver/Driver_monitor.sv
+++ Driver/Driver_monitor.sv
@@ -5,6 +5,7 @@ class driver #(parameter pck_sz=41, drvs=16, id_drv=0);
     event drv_done;
     int wait_;
     int dvc;
+    bit [pck_sz-1:0] fifo[$];
     //int id_drv;
     
     task run();
@@ -16,41 +17,29 @@ class driver #(parameter pck_sz=41, drvs=16, id_drv=0);
          vdc.reset=1;
         @(posedge vdc.clk);
         
-        vdc.data_out_i_in[i]={pck_sz{1'bz}};
         $display("[%g] el driver espera por una transacción",$time);
         wait_=0; 
         @(posedge vdc.clk); 
-        g_a_d_mbx.get(trans);
+        g_a_d_mbx.get(trans);  
         dvc=trans.sender;
         $display("Se recibió una transaccion en el driver, dispositivo=%0d ", dvc);
-      while(wait_ < trans.retardo)begin
+        while(wait_ < trans.retardo)begin
             @(posedge vdc.clk);
             wait_=wait_+1;
-        vdc.data_out_i_in[i] = trans.dato;
+            fifo.push_back(trans.dato);
         end
-      case(trans.modo)
-            0: begin //envío
-                //vdc.push=1;  
-                trans.tiempo=$time;
-                trans.modo=1;
-                d_c_mbx.put(trans);
-                trans.print("Driver: Transacción ejecutada");
-            end
-              endcase
-     case(trans.modo)
-            1: begin //recibido
-              trans.dato=vdc.data_out[i];
-                trans.tiempo=$time;
-                @(posedge vdc.clk);
-              vdc.pop[i]=1;
-                d_c_mbx.put(trans);
-                trans.print("Driver: Transaccion ejecutada");
-            end
-        default: begin
-            $display("Un error de ambiente, id de dispositivo de envio/llegada no coincide",$time);
-            $finish;
-        end
-          endcase
+          
+          if(vdc.pndng_i_in[i])begin//envio
+              vdc.pop_in[i]<=1;
+              vdc.data_out_i_in[i]=fifo.pop_front(trans.dato);
+          end
+      
+          if(vdc.pndng[i])begin//recibido
+            vdc.pop[i]<=1;
+            transaccion #(.pck_sz(pck_sz),.drvs(drvs)) trans=new;
+            trans.dato<=vdc.data_out[i];
+            d_c_mbx.put(trans);
+          end
           end  
         @(posedge vdc.clk);  
         ->drv_done;
